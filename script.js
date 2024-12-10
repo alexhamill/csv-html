68de648a69fb1c2fce7aaac10dc4f55ee3eb905a
alexhamill <alexander.hamill@icloud.com>
2024-12-10 09:15:23 -0500

    starting out and removed the class codes

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 async function fetchCSV() {
-  const response = await fetch('https://raw.githubusercontent.com/alexhamill/csv-html.github.io/refs/heads/main/schedule.csv');
+  const response = await fetch('https://raw.githubusercontent.com/alexhamill/csv-html/refs/heads/main/schdule.csv');
+  //const response = await fetch('https://raw.githubusercontent.com/ArenaPlanning/ArenaPlanning.github.io/refs/heads/main/schedule.csv');
   const csvData = await response.text();
-
+  console.log(csvData);
   const rows = csvData.trim().split('\n').map(row => row.split(','));
 
-
+  
   const classData = {};
   rows.forEach(row => {
     const classCode = row[0];
     const periods = row[1].split(';');
     classData[classCode] = periods;
-    ////console.log(" hello ");
   });
-  consol.log(classData);
+  console.log(classData);
   return classData;
 }
-f
+fetchCSV();
